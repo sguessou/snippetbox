9453760dba165038bfa21bda945b336d2da474ab
Saad Guessous <guessous_saad@hotmail.com>
2022-01-16 18:29:27 +0200

    Add SnippetModel

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/sguessou/snippetbox/pkg/models/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -32,6 +34,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
