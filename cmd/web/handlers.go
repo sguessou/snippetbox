2d22d66ff4687ced332464352392203ff798f20f
Saad Guessous <guessous_saad@hotmail.com>
2022-01-20 22:20:56 +0200

    Add create snippet form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -47,7 +47,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("create a new snippet..."))
+	app.render(w, r, "create.page.tmpl", nil)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
