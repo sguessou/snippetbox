acd4b6f91b499f821a45eb22002a71524d85c1c9
Saad Guessous <guessous_saad@hotmail.com>
2022-01-20 20:38:47 +0200

    Add panic recovery middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,5 +12,5 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.logRequest(secureHeaders(mux))
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
