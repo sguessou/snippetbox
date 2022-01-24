602ee6378098abd97dabeb8c364d3fa33231c71a
Saad Guessous <guessous_saad@hotmail.com>
2022-01-24 20:52:19 +0200

    Set up https server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -60,7 +60,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
