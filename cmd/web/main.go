7ef426b34de518ee23e93c76e433df03c2eb7da3
Saad Guessous <guessous_saad@hotmail.com>
2022-01-25 20:44:40 +0200

    Validate signup form

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,7 @@ type application struct {
 	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
+	users         *mysql.UserModel
 }
 
 func main() {
@@ -52,6 +53,7 @@ func main() {
 		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
+		users:         &mysql.UserModel{DB: db},
 	}
 
 	tlsConfig := &tls.Config{
