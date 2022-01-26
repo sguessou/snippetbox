f41bc8a95abb5af978573733f1f516c51d8d51f7
Saad Guessous <guessous_saad@hotmail.com>
2022-01-26 20:39:22 +0200

    Add login template + handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -122,7 +122,9 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display the user login form...")
+	app.render(w, r, "login.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
