fd88a6f61fd0947cd46c7effec79e2b2acfa377e
Saad Guessous <guessous_saad@hotmail.com>
2022-01-29 09:40:23 +0200

    Add tests for humanDate logic

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -52,7 +52,11 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 }
 
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
