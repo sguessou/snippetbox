86e58b981d39b2dd00cd8ef585ea8801b6d7ed95
Saad Guessous <guessous_saad@hotmail.com>
2022-01-29 19:31:34 +0200

    Add test helpers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d, got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	code, _, body := ts.get(t, "/ping")
+	if code != http.StatusOK {
+		t.Errorf("want %d, got %d", http.StatusOK, code)
 	}
 
 	if string(body) != "OK" {
---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+}
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return rs.StatusCode, rs.Header, body
+}
