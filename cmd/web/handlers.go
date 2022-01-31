b4dced642496243ed86e61eea80c2569bafeae7c
Saad Guessous <guessous_saad@hotmail.com>
2022-01-31 20:40:03 +0200

    Add about page to application

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -27,6 +27,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "about.page.tmpl", nil)
+}
+
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,6 +25,8 @@ func (app *application) routes() http.Handler {
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
 	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
 
+	mux.Get("/about", dynamicMiddleware.ThenFunc(app.about))
+
 	mux.Get("/ping", http.HandlerFunc(ping))
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
